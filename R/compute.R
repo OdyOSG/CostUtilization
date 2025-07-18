8ef02a10f72c892218c636d2f9e2559dbe7c88b7
A1exanderAlexeyuk <alexander_aleksiayuk@epam.com>
2025-07-18 18:55:53 +0300

    add readme, adjust multipe approaches (con, cdm), not properly tested

--- R/compute.R
+++ R/compute.R
@@ -1,43 +1,75 @@
 #' Get Cost Covariate Data
 #'
-#' Computes the cost covariates for a specified cohort based on the provided settings.
+#' Computes cost covariates. This function can calculate costs for a single
+#' cohort or aggregate costs across multiple cohorts.
 #'
-#' @param connection A connection object from the `DatabaseConnector` package.
+#' @param costCovariateSettings A settings object from `createCostCovariateSettings`.
+#' @param cohortId A numeric vector of one or more cohort IDs to analyze.
+#' @param aggregateCohortId If providing multiple `cohortId`s, you must provide a
+#'   single numeric ID to label the aggregated output.
+#' @param cdm A cdm_reference object created by `CDMConnector::cdm_from_con()`.
+#' @param connection A `DatabaseConnector` or `DBI` connection object.
+#' @param connectionDetails A `DatabaseConnector` connectionDetails object.
 #' @param cdmDatabaseSchema The schema holding the OMOP CDM data.
 #' @param cohortDatabaseSchema The schema where the cohort table is located.
 #' @param cohortTable The name of the cohort table.
-#' @param cohortId The ID of the cohort to analyze.
-#' @param costCovariateSettings A settings object created by `createCostCovariateSettings`.
 #'
 #' @return A FeatureExtraction-compatible data frame with the computed covariates.
 #' @export
-getCostCovariateData <- function(connection,
-                                 cdmDatabaseSchema,
-                                 cohortDatabaseSchema,
-                                 cohortTable,
+getCostCovariateData <- function(costCovariateSettings,
                                  cohortId,
-                                 costCovariateSettings) {
-  # Assertions and setup
-  checkmate::assertClass(connection, "DatabaseConnectorConnection")
-  checkmate::assertCharacter(cdmDatabaseSchema, len = 1)
-  checkmate::assertCharacter(cohortDatabaseSchema, len = 1) # New assertion
-  checkmate::assertCharacter(cohortTable, len = 1)
-  checkmate::assertInt(cohortId)
-  checkmate::assertDataFrame(costCovariateSettings)
+                                 aggregateCohortId = NULL,
+                                 cdm = NULL,
+                                 connection = NULL,
+                                 connectionDetails = NULL,
+                                 cdmDatabaseSchema = NULL,
+                                 cohortDatabaseSchema = NULL,
+                                 cohortTable = "cohort") {
   
-  # Run Data Quality Checks
+  # --- Input Validation and Connection Management ---
+  checkmate::assertDataFrame(costCovariateSettings)
+  checkmate::assertNumeric(cohortId, min.len = 1)
+  checkmate::assertInt(aggregateCohortId, null.ok = TRUE)
+  
+  # Check for aggregation logic
+  useAggregation <- length(cohortId) > 1
+  if (useAggregation && is.null(aggregateCohortId)) {
+    stop("When providing multiple cohortIds, you must also provide `aggregateCohortId`.")
+  }
+  outputCohortId <- if (useAggregation) aggregateCohortId else cohortId[1]
+  
+  # Establish connection and schemas from inputs
+  if (!is.null(cdm)) {
+    checkmate::assertR6(cdm, "cdm_reference")
+    connection <- cdm$connection
+    cdmDatabaseSchema <- CDMConnector::cdmSchema(cdm)
+    cohortDatabaseSchema <- attributes(cdm[[cohortTable]])$db_schema
+  } else if (!is.null(connectionDetails)) {
+    checkmate::assertClass(connectionDetails, "connectionDetails")
+    connection <- DatabaseConnector::connect(connectionDetails)
+    on.exit(DatabaseConnector::disconnect(connection))
+  } else if (!is.null(connection)) {
+    checkmate::assertClass(connection, "DBIConnection")
+  } else {
+    stop("Must provide one of 'cdm', 'connectionDetails', or 'connection'.")
+  }
+  
+  if (is.null(cdmDatabaseSchema) || is.null(cohortDatabaseSchema)) {
+    stop("cdmDatabaseSchema and cohortDatabaseSchema must be specified if not using a cdm object.")
+  }
+  
+  # --- Analysis Execution ---
   assertCdmConforms(connection = connection, cdmDatabaseSchema = cdmDatabaseSchema)
   
-  # Render and execute SQL
   sql <- SqlRender::readSql(system.file("sql/getCostCovariates.sql", package = "CostUtilization"))
   
-  # The logic described in the design document is implemented in the SQL query
   renderedSql <- SqlRender::render(
     sql,
     cdm_database_schema = cdmDatabaseSchema,
-    cohort_database_schema = cohortDatabaseSchema, # New parameter
+    cohort_database_schema = cohortDatabaseSchema,
     cohort_table = cohortTable,
-    cohort_id = cohortId,
+    cohort_ids = cohortId, # Changed from cohort_id to cohort_ids
+    output_cohort_id = outputCohortId, # New parameter for labeling
     covariate_id = costCovariateSettings$covariateId,
     cost_concept_id = costCovariateSettings$costConceptId[[1]],
     cost_type_concept_id = costCovariateSettings$costTypeConceptId[[1]],
@@ -49,16 +81,16 @@ getCostCovariateData <- function(connection,
   
   covariates <- DatabaseConnector::querySql(connection, renderedSql, snakeCaseToCamelCase = TRUE)
   
-  # Format output to conform to FeatureExtraction model
+  # Format output
   featureExtractionCovs <- covariates %>%
-    select(
-      cohortDefinitionId = .data$cohortId,
-      subjectId = .data.subjectId,
-      covariateId = .data$covariateId,
+    dplyr::select(
+      cohortDefinitionId = .data$outputCohortId,
+      subjectId = .data$subjectId,
+      covariateId = .data.covariateId,
       covariateValue = .data$sumValue
     ) %>%
-    mutate(timeId = NA) %>%
-    as_tibble()
+    dplyr::mutate(timeId = NA) %>%
+    dplyr::as_tibble()
   
   return(featureExtractionCovs)
 }
--- README.md
+++ README.md
@@ -1,2 +1,90 @@
 # CostUtilization
-[under development] Performs basic cost and utilization characterization analysis
+
+[![Build Status](https://github.com/OHDSI/CostUtilization/workflows/R-CMD-check/badge.svg)](https://github.com/OHDSI/CostUtilization/actions?query=workflow%3AR-CMD-check+branch%3Amain)
+[![codecov.io](https://codecov.io/github/OHDSI/CostUtilization/coverage.svg?branch=main)](https://app.codecov.io/github/OHDSI/CostUtilization?branch=main)
+
+---
+
+## Introduction
+
+This R package provides a standardized framework for calculating costs for specified patient cohorts within an OMOP Common Data Model database.
+
+[cite_start]The package design is based on the principle that cost data requires two key pieces of information for standardization: classification (the "what") and provenance (the "where")[cite: 7, 8, 11]. [cite_start]It provides functions to generate cost covariates by leveraging standard concepts for different cost components (e.g., 'Charged', 'Allowed', 'Paid') [cite: 24] [cite_start]and cost provenance types (e.g., 'Adjudicated Claim')[cite: 78, 81]. [cite_start]The final output conforms to the `FeatureExtraction` package's object model, allowing for seamless integration into existing OHDSI study pipelines[cite: 200].
+
+---
+
+## Examples
+
+You can install and run the package from RStudio. The following example demonstrates how to calculate the total "Allowed" cost for a cohort of patients with Type 2 Diabetes using the Eunomia test dataset.
+
+## Examples
+
+You can install and run the package from RStudio. The following example demonstrates how to calculate the total "Allowed" cost for a cohort of patients with Type 2 Diabetes using the Eunomia test dataset.
+
+```r
+# 1. Install CostUtilization from GitHub
+# install.packages('remotes')
+remotes::install_github('OHDSI/CostUtilization')
+
+# 2. Load the library and its dependencies
+library(CostUtilization)
+library(dplyr)
+library(DatabaseConnector)
+
+# 3. Get connection details for the Eunomia test database
+connectionDetails <- Eunomia::getEunomiaConnectionDetails()
+
+# 4. Create a cohort and sample cost data for the example
+# A helper function to populate the database (implementation not shown for brevity)
+source("[https://raw.githubusercontent.com/OHDSI/CostUtilization/main/extras/TestSetup.R](https://raw.githubusercontent.com/OHDSI/CostUtilization/main/extras/TestSetup.R)")
+setupCostTests(connectionDetails)
+
+
+# 5. Define and execute the cost analysis
+# Define settings to calculate the total "Allowed" cost from "Claims"
+costSettings <- createCostCovariateSettings(
+  covariateId = 1001,
+  costConceptId = c(31978), # Allowed
+  costTypeConceptId = c(32817) # Adjudicated Claim
+)
+
+# Execute the analysis using connectionDetails
+# The function will handle connecting and disconnecting automatically
+costCovariates <- getCostCovariateData(
+  connectionDetails = connectionDetails,
+  cdmDatabaseSchema = "main",
+  cohortDatabaseSchema = "main",
+  cohortTable = "cohort",
+  cohortId = 1, # ID for the T2DM cohort created in setupCostTests()
+  costCovariateSettings = costSettings
+)
+
+# 6. View the results
+print(head(costCovariates))
+```
+
+-----
+
+## Technology
+
+`CostUtilization` is an R package.
+
+-----
+
+## System Requirements
+
+Running the package requires R (version 4.1.0 or higher).
+
+-----
+
+## License
+
+`CostUtilization` is licensed under Apache License 2.0.
+
+-----
+
+## Development
+
+`CostUtilization` is being developed in RStudio. [cite\_start]The package is maintained by the OHDSI community[cite: 199].
+
+
